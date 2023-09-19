1008fcb4647f47fdeb59e4d19424133d0cc39d8c
Titto Solis <solis.a.tito@gmail.com>
2023-09-19 17:36:21 +0900

    Defined check, load and size functions

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,26 @@
+from fileinput import close
+
+
+words = set()
+
 def main():
 
-def check(word):
+    def check(word):
+        if word.lower() in words:
+            return True
+        else:
+            return False
 
-def load(dictionary):
+    def load(dictionary):
+        file = open(dictionary, "r")
+        for line in file:
+            word = line.rstrip()
+            words.add(word)
+        close(file)
+        return True
 
-def size():
-
+    def size():
+        return len(words)
     
 if __name__ == "__main__":
     main()
