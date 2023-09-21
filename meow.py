2b62f27a15b5dd02010bd2e1573c316e62ee70f8
Titto Solis <solis.a.tito@gmail.com>
2023-09-21 10:17:17 +0900

    meow program upload - clean code documentation

---
+++ meow.py
@@ -0,0 +1,16 @@
+#-----Juicy part of the program------#
+
+def main():
+    for i in range(3):
+        meow() #calls meow() which is defined below.
+
+
+#-----Function Definition Space-----#
+
+#meow() function definition
+def meow():
+    print("meow")
+
+
+#-----------Call main()------------#
+main()
