7abc441bdf56858dbb54934d29bffde66f8e685a
Titto Solis <solis.a.tito@gmail.com>
2023-09-21 10:36:31 +0900

    Ask the cat to meow n # of times.

--- meow.py
+++ meow.py
@@ -1,15 +1,16 @@
 #-----Juicy part of the program------#
 
 def main():
-    for i in range(3):
-        meow() #calls meow() which is defined below.
+    userinput = int(input("How many times should the cat meow? ")) #Requests user input and only access numbers
+    meow(userinput) #calls meow() which is defined below.
 
 
 #-----Function Definition Space-----#
 
 #meow() function definition
-def meow():
-    print("meow")
+def meow(n):
+    for i in range(n): #Loops through n-times. n is dynamic, set by the user input defined in main()
+        print("meow")
 
 
 #-----------Call main()------------#
